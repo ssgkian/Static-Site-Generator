09347ef3981d8eb14474e00bd22eb648a2916209
ssgkian <226536320+ssgkian@users.noreply.github.com>
2025-12-31 12:17:06 +0000

    Static-Site-Generator project complete

--- src/gencontent.py
+++ src/gencontent.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from markdown_blocks import extract_title, markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     dir_path = os.path.dirname(dest_path)
     if dir_path != "":
@@ -19,18 +19,20 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     page = template.replace("{{ Title }}", title)
     page = page.replace("{{ Content }}", html_string)
+    page = page.replace('href="/', f'href="{base_path}')
+    page = page.replace('src="/', f'src="{base_path}')
 
     with open(dest_path, "w") as f:
         f.write(page)
 
 
-def generate_pages_recursive(content_dir, template_path, dest_dir):
+def generate_pages_recursive(content_dir, template_path, dest_dir, base_path):
     content_dir = Path(content_dir)
     dest_dir = Path(dest_dir)
     for path in content_dir.iterdir():
         dest_path = dest_dir / path.name
         if path.is_dir():
-            generate_pages_recursive(path, template_path, dest_path)
+            generate_pages_recursive(path, template_path, dest_path, base_path)
         elif path.is_file() and path.suffix == ".md":
             dest_htmlpath = dest_dir / (path.stem + ".html")
-            generate_page(path, template_path, dest_htmlpath)
+            generate_page(path, template_path, dest_htmlpath, base_path)
--- src/main.py
+++ src/main.py
@@ -1,21 +1,28 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_dir
 from gencontent import generate_pages_recursive
 
 
 def main():
+    base_path = None
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+    else:
+        base_path = "/"
+
     static_dir = "./static"
-    public_dir = "./public"
+    docs_dir = "./docs"
     content_dir = "./content"
     template_path = "./template.html"
-    if os.path.exists(public_dir):
+    if os.path.exists(docs_dir):
         print("DELETING PUBLIC DIRECTORY")
-        shutil.rmtree(public_dir)
-    os.mkdir(public_dir)
-    copy_dir(static_dir, public_dir)
-    generate_pages_recursive(content_dir, template_path, public_dir)
+        shutil.rmtree(docs_dir)
+    os.mkdir(docs_dir)
+    copy_dir(static_dir, docs_dir)
+    generate_pages_recursive(content_dir, template_path, docs_dir, base_path)
 
 
 main()
